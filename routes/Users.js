9b9b50d05238ddf41ecca38c3a39afbd1c8470e2
paycheck97 <43653779+paycheck97@users.noreply.github.com>
2019-05-27 17:21:17 -0400

    express r e a d y

---
+++ routes/Users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database');
+
+
+router.get('/example', async  (req, res) => {
+    console.log('por aqui pase');
+    try{
+        const pais = await pool.query('SELECT * FROM pais');
+        const ciudad = await pool.query('SELECT * FROM ciudad');
+        const aeropuerto = await pool.query('SELECT * FROM aeropuerto');
+        res.json(pais, ciudad, aeropuerto);
+    } catch (e){
+        console.log(e);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');   //Muestra peticiones en consola
-const cors = requier('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const sesion = require('express-session');
+const bd = require('express-mysql-session');
+const {database } = require('./keys');
 
 const app = express();
 app.set('port', process.env.PORT || 5000);
 
-
-
 //middleware
+app.use(sesion({
+    secret: "pipo",
+    resave: false,
+    saveUninitialized: false,
+    store: new bd(database),
+
+}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(cors());
+app.use(flash());
 
 //globales
 app.use((req, res, next) =>{
@@ -23,7 +34,6 @@ app.use((req, res, next) =>{
 });
 
 //routes
-var Users = require('./routes/Users')
 app.use(require('./routes/Users'));
 
 
@@ -32,3 +42,4 @@ app.listen(app.get('port'), () =>{
     console.log('server on port', app.get('port'));
 
 });
+
