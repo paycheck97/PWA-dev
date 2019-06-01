56d0a19af2f3c1502091925892bc79d251b3d7b9
Guido <guido23acuna@gmail.com>
2019-06-01 10:49:06 -0400

    guido3

--- keys.js
+++ keys.js
@@ -3,7 +3,7 @@ module.exports = {
     database : {
         host: 'localhost',
         user: 'root',
-        password: 'password',
+        password: 'guido',
         database: 'souschef',
     }
     
