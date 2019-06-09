6648b1c6a2e203ea8bc15d0d8c6a8f5a84a48a36
alfredojahn <jahn4563@gmail.com>
2019-06-09 19:52:20 -0400

    Cambio Login

--- keys.js
+++ keys.js
@@ -1,10 +1,8 @@
 module.exports = {
-
-    database : {
-        host: 'localhost',
-        user: 'root',
-        password: 'guido',
-        database: 'souschef',
-    }
-    
+  database: {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "souschef"
+  }
 };
