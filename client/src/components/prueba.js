0540ade2a9d2b2ddc2f5680f80b9acd51eaa1e68
Guido <guido23acuna@gmail.com>
2019-06-09 23:28:18 -0400

    ver recetas

--- client/src/components/prueba.js
+++ client/src/components/prueba.js
@@ -3,6 +3,7 @@ import "./prueba.css";
 import { Link } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
 import { Typography } from "@material-ui/core";
+import {Consumer} from './context';
 
 class prueba extends Component {
   state = {
@@ -20,16 +21,12 @@ class prueba extends Component {
       );
   }
 
-  getID = (id) => {
-    console.log(id)
-    
-  }
+  
 
   render() {
     const { recetas } = this.state;
-    var id = null;
-    return (
-      <div className="container text-center my-3">
+          return (
+            <div className="container text-center my-3">
         <div className="row mx-auto my-auto">
           <div>
             <Carousel indicators={false}>
@@ -46,7 +43,7 @@ class prueba extends Component {
                       <Typography>Tiempo de preparacion {receta.prep_time}</Typography>
                       <Typography>Calorias {receta.calories_ps}</Typography>
                       <Typography>Servings {receta.servings}</Typography>
-                      <Link to="/Edit" className="btn btn-primary" onClick={this.getID({id})}>
+                      <Link to={`Edit/${receta.id}`} className="btn btn-primary" >
                         Edit
                       </Link>
                     </div>
@@ -57,7 +54,7 @@ class prueba extends Component {
           </div>
         </div>
       </div>
-    );
+          )
   }
 }
 
